d5abbdd23c7677632eed81189e1c27307b99620f
MistaBasco <mistabasco>
2024-09-03 11:14:28 +0100

    added a form to do post/get stuff

---
+++ client/app.js
@@ -0,0 +1,26 @@
+const messageForm = document.querySelector("#message-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  console.log("Form Submitted!");
+  //do stuff with form data here
+
+  const formData = new FormData(messageForm);
+  const message = formData.get("message");
+
+  const promise = await fetch(
+    "https://mono-repo-8mku.onrender.com:3000/messages",
+    {
+      method: "POST", //this is where we set the HTTP method i.e, POST
+      headers: {
+        "Content-Type": "application/json", // This tels the server that we're sending stringified JSON
+      },
+      body: JSON.stringify({ message }),
+    }
+  );
+  const promiseData = await promise.json();
+  console.log("From the server:", promiseData);
+  messageForm.reset();
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ app.get("/", (request, response) => {
   response.json("you are looking at my root route, how roude!");
 });
 
+app.post("/messages", function (req, res) {
+  console.log("req.body:", req.body);
+  res.json({ status: "Message Received!" });
+});
+
 app.listen(8080, () => {
   console.log("App is listening on port 8080");
 });
