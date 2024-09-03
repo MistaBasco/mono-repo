a8b13ea4739e9dad792a98142dec7a56e8f85539
MistaBasco <mistabasco>
2024-09-03 15:53:42 +0100

    fixed all the bs i think

--- client/app.js
+++ client/app.js
@@ -1,4 +1,8 @@
 const messageForm = document.querySelector("form");
+const serverURL = "https://mono-repo-8mku.onrender.com";
+///const serverURL = "http://localhost";
+
+console.log("enter...");
 
 async function handleSubmit(event) {
   event.preventDefault();
@@ -9,16 +13,13 @@ async function handleSubmit(event) {
   //   const message = formData.get("message");
   const message = Object.fromEntries(formData);
 
-  const promise = await fetch(
-    "https://mono-repo-8mku.onrender.com:3000/messages",
-    {
-      method: "POST", //this is where we set the HTTP method i.e, POST
-      headers: {
-        "Content-Type": "application/json", // This tels the server that we're sending stringified JSON
-      },
-      body: JSON.stringify({ message }),
-    }
-  );
+  const promise = await fetch(`${serverURL}:8080/messages`, {
+    method: "POST", //this is where we set the HTTP method i.e, POST
+    headers: {
+      "Content-Type": "application/json", // This tels the server that we're sending stringified JSON
+    },
+    body: JSON.stringify({ message }),
+  });
   const promiseData = await promise.json();
   console.log("From the server:", promiseData);
   messageForm.reset();
